13820f1e1a8ed525d25503de19194e25788834d3
AYDIN <AYDIN@DESKTOP-5TRD7K4>
2023-09-11 18:13:26 +0300

    Add project files.

---
+++ 01.MyFirstConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace _01.MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı Tahmini Oyununa Hoşgeldiniz");
+            Random random = new Random();
+            int TargetNumber = random.Next(1, 101);
+
+            while (true)
+            {
+                Console.WriteLine("Hazırsanız 1 ile 101 Arasında Bir Sayı Girin Ve Tuttuğum Sayıyı Bulmaya Çalışın");
+                string input = Console.ReadLine();
+                int myGuess = Convert.ToInt32(input);
+
+                if (myGuess == TargetNumber)
+                {
+                    Console.WriteLine("Tebrik Ederimm Tuttuğum Sayıyı Bildiniz :)");
+                    Console.ReadLine();
+                    Console.WriteLine("Tekrar Oynamak İster Misiniz?");
+
+                }
+                else if (myGuess > TargetNumber)
+                {
+                    if (myGuess - 1 == TargetNumber)
+                    {
+                        Console.WriteLine("Vay Be Çok Yaklaştın Senin Yerinde Olsaydım Sayıyı Bir Azaltırdım");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tuttuğum Sayıdan Daha Büyük Bir Tahminde Bulundunuz");
+                    }
+                }
+                else if (myGuess < TargetNumber)
+                {
+                    if (myGuess + 1 == TargetNumber)
+                    {
+                        Console.WriteLine("Hmm Çok Yaklaştın Ben Sen Olsaydım Eğer Sayıyı Bir Arttırırdım");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tuttuğum sayıdan daha küçük bir tahminde bulundunuz");
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,47 @@
+# BT Akademi and CareetHuBT - .Net Developer Eðitimi
+
+
+## 1.Gün Ýþlenen Konular
+- Sektörler
+- Yazýlým Dilleri
+- Yazýlým Dillerine Göre Seçilebilecek Meslekler ve Maaþ Beklentileri
+- Visual Studio Kurulumu
+- C# Dili
+- VS Aracýnýn Neden Gerekli Olduðu
+- Örnek Console App
+- Bazý Temel Deðiþkenler
+- Örnek Bir Type Convert
+- Örnek Bir Sayý Tahmini Console App
+- Markdown Kodlamadan Kýsaca Bahsedildi
+
+
+
+# BT Academy and CareetHuBT - .Net Developer Training
+
+
+
+
+
+## Topics covered on Day 1
+
+- Sectors
+
+- Software Languages
+
+- Occupations and Salary Expectations According to Software Languages
+
+- Visual Studio Installation
+
+- C# Language
+
+- Why the VS Tool is Necessary
+
+- Sample Console App
+
+- Some Basic Variables
+
+- An Example Type Convert
+
+- An Example Number Prediction Console App
+
+- Markdown Coding Briefly Mentioned
